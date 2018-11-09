ed27d5993090353d203cdd91d83c4864aaba5fea
Breno Nunes <breno.arantes97@gmail.com>
2018-11-08 23:09:21 -0300

    Upgrade #22
    Changed to vuejs framework;

---
+++ src/components/DependencyChart.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="mynetwork"></div>
+</template>
+
+<script>
+export default {
+    name: 'DependencyChart'
+}
+</script>
+
+<style>
+    #mynetwork {
+        border: 2px solid lightgray;
+    }
+</style>
