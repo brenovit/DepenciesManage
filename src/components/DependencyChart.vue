f3887a1e2a759f6688115b93764cd9dad3f46c8b
Breno Nunes <breno.arantes97@gmail.com>
2018-11-09 09:41:42 -0300

    Upgrade #24
    Removed app.js server;

--- src/components/DependencyChart.vue
+++ src/components/DependencyChart.vue
@@ -19,85 +19,85 @@ export default {
     components : {
         Network
     },
-    data () {
+    data: function () {
         return {
-        colorTomcat : {
-            background: "#FF9800",
-            border: "#616161",
-            hover: {
-                background: "#F57C00",
-                border: "#424242"
+            colorTomcat : {
+                background: "#FF9800",
+                border: "#616161",
+                hover: {
+                    background: "#F57C00",
+                    border: "#424242"
+                },
+                highlight: {
+                    background: "#E65100",
+                    border: "#212121"
+                }
             },
-            highlight: {
-                background: "#E65100",
-                border: "#212121"
-            }
-        },
-        colorSocketRmi : {
-            background: "#f44336",
-            border: "#616161",
-            hover: {
-                background: "#d32f2f",
-                border: "#424242"
+            colorSocketRmi : {
+                background: "#f44336",
+                border: "#616161",
+                hover: {
+                    background: "#d32f2f",
+                    border: "#424242"
+                },
+                highlight: {
+                    background: "#b71c1c",
+                    border: "#212121"
+                }
             },
-            highlight: {
-                background: "#b71c1c",
-                border: "#212121"
-            }
-        },
-        colorGlassfish : {
-            background: "#03A9F4",
-            border: "#616161",
-            hover: {
-                background: "#0288D1",
-                border: "#424242"
+            colorGlassfish : {
+                background: "#03A9F4",
+                border: "#616161",
+                hover: {
+                    background: "#0288D1",
+                    border: "#424242"
+                },
+                highlight: {
+                    background: "#01579B",
+                    border: "#212121"
+                }
             },
-            highlight: {
-                background: "#01579B",
-                border: "#212121"
-            }
-        },
-        colorDependencies : {
-            background: "#607D8B",
-            border: "#616161",
-            hover: {
-                background: "#455A64",
-                border: "#424242"
+            colorDependencies : {
+                background: "#607D8B",
+                border: "#616161",
+                hover: {
+                    background: "#455A64",
+                    border: "#424242"
+                },
+                highlight: {
+                    background: "#263238",
+                    border: "#212121"
+                }
             },
-            highlight: {
-                background: "#263238",
-                border: "#212121"
-            }
-        },
-        colorDesktop : {
-            background: "#8BC34A",
-            border: "#616161",
-            hover: {
-                background: "#689F38",
-                border: "#424242"
+            colorDesktop : {
+                background: "#8BC34A",
+                border: "#616161",
+                hover: {
+                    background: "#689F38",
+                    border: "#424242"
+                },
+                highlight: {
+                    background: "#33691E",
+                    border: "#212121"
+                }
             },
-            highlight: {
-                background: "#33691E",
-                border: "#212121"
+            colorUndefined : {
+                background: "#BDBDBD",
+                border: "#616161",
+                hover: {
+                    background: "#9E9E9E",
+                    border: "#424242"
+                },
+                highlight: {
+                    background: "#757575",
+                    border: "#212121"
+                }
             }
-        },
-        colorUndefined : {
-            background: "#BDBDBD",
-            border: "#616161",
-            hover: {
-                background: "#9E9E9E",
-                border: "#424242"
-            },
-            highlight: {
-                background: "#757575",
-                border: "#212121"
-            }
-        }
         }
     },
     network : {
         nodes :[
-            { id: "ACID", label: "br.com.unik\nACID\n1.0.7", color: this.$data.colorDependencies },
+            { id: "ACID", label: "br.com.unik\nACID\n1.0.7", color: this.colorDependencies },
             { id: "APPGUIMENU", label: "br.com.rtkomp\nAPPGUIMENU\n0010807", color: this.colorDesktop },
             { id: "APPWEBFACES", label: "br.com.rtkomp\nAPPWEBFACES\n0010101", color: this.colorTomcat },
             { id: "EAAtendimento", label: "br.com.rtkomp\nEAAtendimento-EJB\n0010808", color: this.colorGlassfish },
